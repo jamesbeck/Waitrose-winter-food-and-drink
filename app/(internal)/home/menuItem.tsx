6913bf7c8c5b43b200e2d28a08e6a8da49ca07e3
Tim Wardle <tim@trycatchsoftware.co.uk>
2024-10-14 16:26:24 +0100

    Homepage menu

---
+++ app/(internal)/home/menuItem.tsx
@@ -0,0 +1,92 @@
+import { cn } from '@/lib/utils';
+import type { StaticImageData } from 'next/image';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+type Colour = 'red' | 'green' | 'brown';
+
+type Props = {
+  text: string;
+  icon?: React.ReactNode;
+  image?: StaticImageData;
+  href?: string;
+  align: 'left' | 'right';
+  colours: [Colour, Colour, Colour];
+};
+
+export const MenuItem: React.FC<Props> = ({
+  text,
+  icon,
+  image,
+  href,
+  align,
+  colours,
+}: Props) => {
+  const colourClasses = colours.map((colour) => {
+    switch (colour) {
+      case 'red':
+        return 'bg-waitrose-red text-waitrose-brown';
+      case 'green':
+        return 'bg-waitrose-lime text-waitrose-red';
+      case 'brown':
+        return 'bg-waitrose-brown text-waitrose-red';
+    }
+  });
+
+  const content = (
+    <>
+      <div className="flex min-h-48">
+        <div
+          className={cn(
+            align === 'left' ? 'grow relative' : 'w-16',
+            align === 'left' ? '' : colourClasses[0]
+          )}
+        >
+          {align === 'left' && image ? (
+            <Image src={image} alt={text} fill className="object-cover" />
+          ) : (
+            ''
+          )}
+        </div>
+        <div
+          className={cn(
+            align === 'left' ? 'w-16' : 'grow relative',
+            align === 'left' ? colourClasses[0] : ''
+          )}
+        >
+          {align === 'right' && image ? (
+            <Image src={image} alt={text} fill className="object-cover" />
+          ) : (
+            ''
+          )}
+        </div>
+      </div>
+
+      <div className="flex h-16">
+        <div
+          className={cn(
+            'flex justify-center items-center',
+            align === 'left'
+              ? 'grow uppercase text-2xl flex justify-center items-center'
+              : 'w-16',
+            colourClasses[1]
+          )}
+        >
+          {align === 'left' ? text : icon}
+        </div>
+        <div
+          className={cn(
+            'flex justify-center items-center',
+            align === 'left' ? 'w-16' : 'grow uppercase text-2xl',
+            colourClasses[2]
+          )}
+        >
+          {align === 'left' ? icon : text}
+        </div>
+      </div>
+    </>
+  );
+
+  return href ? <Link href={href}>{content}</Link> : <>{content}</>;
+};
--- app/(internal)/home/page.tsx
+++ app/(internal)/home/page.tsx
@@ -1,11 +1,64 @@
+import { ZoomIcon } from '@/components/icons/zoomIcon';
+import HeaderImage from '@/images/header.png';
+import MasterclassImage from '@/images/masterclass.png';
+import ProductsImage from '@/images/products.png';
+import ScheduleImage from '@/images/schedule.png';
+import WhatsOnImage from '@/images/whats-on.png';
+import WishlistImage from '@/images/wishlist.png';
+import Image from 'next/image';
+import { MenuItem } from './menuItem';
+
 export default function Home() {
   return (
     <div>
-      <div className="bg-waitrose-red w-full h-20"></div>
-      <div className="bg-waitrose-brown w-full h-20"></div>
-      <div className="bg-waitrose-beige w-full h-20"></div>
-      <div className="bg-waitrose-lime w-full h-20"></div>
-      <div className="bg-waitrose-green w-full h-20"></div>
+      <Image src={HeaderImage} alt="header" priority />
+
+      <MenuItem
+        text="Festival Map"
+        align="left"
+        colours={['red', 'green', 'brown']}
+        icon={<ZoomIcon />}
+      />
+
+      <MenuItem
+        text="What's On"
+        align="right"
+        colours={['brown', 'green', 'red']}
+        image={WhatsOnImage}
+        href="/whats-on"
+      />
+
+      <MenuItem
+        text="My Schedule"
+        align="left"
+        colours={['green', 'brown', 'red']}
+        image={ScheduleImage}
+        href="/schedule"
+      />
+
+      <MenuItem
+        text="Masterclass"
+        align="right"
+        colours={['red', 'brown', 'green']}
+        image={MasterclassImage}
+        href="/masterclasses"
+      />
+
+      <MenuItem
+        text="My Wishlist"
+        align="left"
+        colours={['red', 'green', 'brown']}
+        image={WishlistImage}
+        href="/wishlist"
+      />
+
+      <MenuItem
+        text="Products"
+        align="right"
+        colours={['red', 'green', 'brown']}
+        image={ProductsImage}
+        href="/products"
+      />
     </div>
   );
 }
