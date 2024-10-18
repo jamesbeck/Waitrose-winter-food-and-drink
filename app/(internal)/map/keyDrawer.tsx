1d5d6cc61fc17f9bf63fe271d0d8586a3e532af4
Tim Wardle <tim@trycatchsoftware.co.uk>
2024-10-18 14:51:31 +0100

    Version tweaks

--- app/(internal)/map/keyDrawer.tsx
+++ app/(internal)/map/keyDrawer.tsx
@@ -32,84 +32,88 @@ export const KeyDrawer: React.FC<Props> = (props: Props) => {
 
       <DrawerContent
         overlay={false}
-        className="p-6 left-0 space-y-12 top-16 bottom-0 mt-0 rounded-none border-none w-full overflow-y-auto overflow-x-hidden"
+        className="p-6 left-0 space-y-12 top-16 bottom-0 mt-0 rounded-none border-none w-full"
       >
-        <DialogHeader className="relative">
-          <Button
-            variant="ghost"
-            className="absolute left-0 top-1.5 w-fit h-fit p-0"
-            onClick={() => setOpen(false)}
-          >
-            <BackIcon />
-          </Button>
+        <div className="overflow-y-auto overflow-x-hidden">
+          <DialogHeader className="relative">
+            <Button
+              variant="ghost"
+              className="absolute left-0 top-1.5 w-fit h-fit p-0"
+              onClick={() => setOpen(false)}
+            >
+              <BackIcon />
+            </Button>
 
-          <div>
-            <DialogTitle>Key to map</DialogTitle>
+            <div>
+              <DialogTitle>Key to map</DialogTitle>
 
-            <DialogDescription className="hidden">Key to map</DialogDescription>
-          </div>
-
-          <div className="py-12 space-y-6">
-            <div className="flex space-x-6 items-center">
-              <div className="size-10 bg-[#E0B1C6]"></div>
-              <div className="uppercase">Wine</div>
+              <DialogDescription className="hidden">
+                Key to map
+              </DialogDescription>
             </div>
 
-            <div className="flex space-x-6 items-center">
-              <div className="size-10 bg-[#9FCDE1]"></div>
-              <div className="uppercase">Spirits</div>
-            </div>
-
-            <div className="flex space-x-6 items-center">
-              <div className="size-10 bg-[#ECCF83]"></div>
-              <div className="uppercase">Beer &amp; Cider</div>
-            </div>
-
-            <div className="flex space-x-6 items-center">
-              <div className="size-10 bg-[#9EC498]"></div>
-              <div className="uppercase">Cosy Corner</div>
-            </div>
-
-            <div className="flex space-x-6 items-center">
-              <div className="size-10 bg-[#B08D84]"></div>
-              <div className="uppercase">Mindful Drinking Den</div>
-            </div>
-
-            <div className="flex space-x-6 items-center">
-              <div className="size-10 bg-[#A99CC5]"></div>
-              <div className="uppercase">Waitrose Experiences</div>
-            </div>
-
-            <div className="flex space-x-6 items-center">
-              <div className="size-10 relative">
-                <Image src={MapGoodyBagImage} alt="Goody bag" fill />
+            <div className="pt-12 space-y-6">
+              <div className="flex space-x-6 items-center">
+                <div className="size-10 bg-[#E0B1C6]"></div>
+                <div className="uppercase">Wine</div>
               </div>
 
-              <div className="uppercase">Goody Bag Collection Point</div>
-            </div>
-
-            <div className="flex space-x-6 items-center">
-              <div className="size-10 relative">
-                <Image
-                  src={MapGlassWashPointImage}
-                  alt="Glass wash point"
-                  fill
-                />
+              <div className="flex space-x-6 items-center">
+                <div className="size-10 bg-[#9FCDE1]"></div>
+                <div className="uppercase">Spirits</div>
               </div>
-              <div className="uppercase">Glass Wash Point</div>
-            </div>
 
-            <div className="flex space-x-6 items-center">
-              <div className="size-10 bg-[#ED67A5]"></div>
-              <div className="uppercase">Toilets</div>
-            </div>
+              <div className="flex space-x-6 items-center">
+                <div className="size-10 bg-[#ECCF83]"></div>
+                <div className="uppercase">Beer &amp; Cider</div>
+              </div>
 
-            <div className="flex space-x-6 items-center">
-              <div className="size-10 bg-[#DC8C72]"></div>
-              <div className="uppercase">Facilities</div>
+              <div className="flex space-x-6 items-center">
+                <div className="size-10 bg-[#9EC498]"></div>
+                <div className="uppercase">Cosy Corner</div>
+              </div>
+
+              <div className="flex space-x-6 items-center">
+                <div className="size-10 bg-[#B08D84]"></div>
+                <div className="uppercase">Mindful Drinking Den</div>
+              </div>
+
+              <div className="flex space-x-6 items-center">
+                <div className="size-10 bg-[#A99CC5]"></div>
+                <div className="uppercase">Waitrose Experiences</div>
+              </div>
+
+              <div className="flex space-x-6 items-center">
+                <div className="size-10 relative">
+                  <Image src={MapGoodyBagImage} alt="Goody bag" fill />
+                </div>
+
+                <div className="uppercase">Goody Bag Collection Point</div>
+              </div>
+
+              <div className="flex space-x-6 items-center">
+                <div className="size-10 relative">
+                  <Image
+                    src={MapGlassWashPointImage}
+                    alt="Glass wash point"
+                    fill
+                  />
+                </div>
+                <div className="uppercase">Glass Wash Point</div>
+              </div>
+
+              <div className="flex space-x-6 items-center">
+                <div className="size-10 bg-[#ED67A5]"></div>
+                <div className="uppercase">Toilets</div>
+              </div>
+
+              <div className="flex space-x-6 items-center">
+                <div className="size-10 bg-[#DC8C72]"></div>
+                <div className="uppercase">Facilities</div>
+              </div>
             </div>
-          </div>
-        </DialogHeader>
+          </DialogHeader>
+        </div>
       </DrawerContent>
     </Drawer>
   );
