25cee62b7fa75db408b936808596f65d64a51a54
James Beck <james@jamesbeck.co.uk>
2024-11-13 14:01:55 +0000

    fixed wishlist search

--- lib/data/wishlist.ts
+++ lib/data/wishlist.ts
@@ -1,11 +1,11 @@
-'use server';
+"use server";
 
-import type { Product } from 'knex/types/tables';
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
-import { db } from '../knex';
-import type { ProductWithWishlisted } from './products';
-import { getCurrentUser } from './user';
+import type { Product } from "knex/types/tables";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { db } from "../knex";
+import type { ProductWithWishlisted } from "./products";
+import { getCurrentUser } from "./user";
 
 type Params = {
   offset?: number;
@@ -19,33 +19,33 @@ export const getWishlist = async ({
   const user = await getCurrentUser();
 
   if (!user) {
-    return redirect('/log-in');
+    return redirect("/log-in");
   }
 
   const baseQuery = db
-    .from('wishlist')
-    .where('wishlist.user_id', user.id)
+    .from("wishlist")
+    .where("wishlist.user_id", user.id)
     .leftJoin(
-      'products',
-      'products.line_number',
-      '=',
-      'wishlist.product_line_number'
+      "products",
+      "products.line_number",
+      "=",
+      "wishlist.product_line_number"
     );
 
   if (search) {
     baseQuery.andWhere((builder) => {
       builder
-        .whereILike('products.supplier', `%${search}%`)
-        .orWhere('products.room', search);
+        .whereILike("products.supplier", `%${search}%`)
+        .orWhereILike("products.name", `%${search}%`);
     });
   }
 
   const [count, items] = await Promise.all([
-    baseQuery.clone().count<{ count: string }[]>('*').first(),
+    baseQuery.clone().count<{ count: string }[]>("*").first(),
     baseQuery
       .clone()
       .select<ProductWithWishlisted[]>([
-        'products.*',
+        "products.*",
         db.raw('true as "is_wishlisted"'),
       ])
       .offset(offset)
@@ -56,38 +56,38 @@ export const getWishlist = async ({
 };
 
 export const addProductToWishlist = async (
-  line_number: Product['line_number']
+  line_number: Product["line_number"]
 ) => {
   const user = await getCurrentUser();
 
   if (!user) {
-    return redirect('/log-in');
+    return redirect("/log-in");
   }
 
   await db
-    .table('wishlist')
+    .table("wishlist")
     .insert({ user_id: user.id, product_line_number: line_number });
 
-  revalidatePath('/wishlist');
+  revalidatePath("/wishlist");
 
   return true;
 };
 
 export const removeProductFromWishlist = async (
-  line_number: Product['line_number']
+  line_number: Product["line_number"]
 ) => {
   const user = await getCurrentUser();
 
   if (!user) {
-    return redirect('/log-in');
+    return redirect("/log-in");
   }
 
   await db
-    .table('wishlist')
+    .table("wishlist")
     .where({ user_id: user.id, product_line_number: line_number })
     .delete();
 
-  revalidatePath('/wishlist');
+  revalidatePath("/wishlist");
 
   return true;
 };
